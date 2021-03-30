057276683ef5dcdb9dec34a47dd424d4ee5eef36
yulya.podolyako <podolyako.yulya@mail.ru>
2021-03-30 21:59:53 +0300

    bokb

---
+++ Front-end/Login/js/log.js
@@ -0,0 +1,39 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+$('body').on('click', '.password-control', function(){
+	if ($('#password-input').attr('type') == 'password'){
+		$(this).addClass('view');
+		$('#password-input').attr('type', 'text');
+	} else {
+		$(this).removeClass('view');
+		$('#password-input').attr('type', 'password');
+	}
+	return false;
+});
+
+
+function studentCheck() {
+    document.getElementsByClassName('student').style.display = 'block';
+	document.getElementsByClassName('teacher').style.display = 'none';
+}
+
+
+
