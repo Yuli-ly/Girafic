4a1949733aebf4eae7e266e19194201067421afb
Anatoli Afonenko <anatolii92111@gmail.com>
2021-04-09 13:07:48 +0300

    Start back-end development. Created project

--- Front-end/Login/js/log.js
+++ Front-end/Login/js/log.js
@@ -1,38 +1,38 @@
 const inputs = document.querySelectorAll(".input");
 
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
+function addcl() {
+    let parent = this.parentNode.parentNode;
+    parent.classList.add("focus");
 }
 
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
+function remcl() {
+    let parent = this.parentNode.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
 }
 
 inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+    input.addEventListener("focus", addcl);
+    input.addEventListener("blur", remcl);
 });
 
-$('body').on('click', '.password-control', function(){
-	if ($('#password-input').attr('type') == 'password'){
-		$(this).addClass('view');
-		$('#password-input').attr('type', 'text');
-	} else {
-		$(this).removeClass('view');
-		$('#password-input').attr('type', 'password');
-	}
-	return false;
+$('body').on('click', '.password-control', function () {
+    if ($('#password-input').attr('type') == 'password') {
+        $(this).addClass('view');
+        $('#password-input').attr('type', 'text');
+    } else {
+        $(this).removeClass('view');
+        $('#password-input').attr('type', 'password');
+    }
+    return false;
 });
 
 
 function studentCheck() {
     document.getElementsByClassName('student').style.display = 'block';
-	document.getElementsByClassName('teacher').style.display = 'none';
+    document.getElementsByClassName('teacher').style.display = 'none';
 }
 
 
